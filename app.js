533cf7eedf090c8b7e5f4fa42313ae6042f08c77
Manuel Balbás <manuelbalbas@Manuels-MacBook-Pro.local>
2019-10-06 13:42:43 +0200

    reading images with tesseract working. Checked docs to make it work

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const app = express();
 const fs = require('fs');
 const multer = require('multer');
 const { createWorker } = require('tesseract.js');
-const worker = createWorker();
+const worker = createWorker({
+  logger: m => console.log(m)
+});
 
 // Storage
 const storage = multer.diskStorage({
-  destination: (req, res, cb) => {
+  destination: (req, file, cb) => {
     cb(null, './uploads');
   },
-  filename: (req, res, cb) => {
-    cb(null, req.file);
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
   }
 });
 const upload = multer({ storage: storage }).single('test');
@@ -22,6 +24,22 @@ app.set('vew engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
+});
+
+app.post('/upload', (req, res) => {
+  upload(req, res, err => {
+    (async () => {
+      await worker.load();
+      await worker.loadLanguage('eng');
+      await worker.initialize('eng');
+      const {
+        data: { text }
+      } = await worker.recognize(`./uploads/${req.file.originalname}`);
+      res.send(text);
+
+      await worker.terminate();
+    })();
+  });
 });
 
 // Start Up our server
